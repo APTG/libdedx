2328cf4e4be52d4a36e2fa3f72fa84e2b81ddfb2
Jakob Toftegaard <jakob.toftegaard@gmail.com>
2012-03-08 21:23:27 +0000

    added get_dedx to python wrapper

--- python/libdedx.c
+++ python/libdedx.c
@@ -5,16 +5,35 @@ static PyObject * get_simple_dedx(PyObject *self, PyObject *args)
 {
 	int ion,target,err;
 	float energy;
-	int ok;
 	float stp;
-	ok = PyArg_ParseTuple(args, "llf", &ion, &target, &energy);
+	PyArg_ParseTuple(args, "llf", &ion, &target, &energy);
 
 	stp = dedx_get_simple_stp(ion,target,energy,&err);
 	return PyFloat_FromDouble((double)stp);
 }
+static PyObject * get_dedx(PyObject *self, PyObject *args)
+{
+	int prog,ion,target,err;
+	float energy;
+	float stp;
+	dedx_config * config;
+	dedx_workspace *ws;
+	PyArg_ParseTuple(args, "lllf",&prog, &ion, &target, &energy);
+	config = calloc(1,sizeof(dedx_config));
+	ws = dedx_allocate_workspace(1,&err);
+	config->program = prog;
+	config->ion = ion;
+	config->target = target;
+	dedx_load_config(ws,config,&err);
+	stp = dedx_get_stp(ws,config,energy,&err);
+	
+	dedx_free_workspace(ws,&err);
+	dedx_free_config(config,&err);
+	return PyFloat_FromDouble((double)stp);
+}
 static PyMethodDef LibdedxMethods[] = {
-    {"get_dedx",  get_simple_dedx, METH_VARARGS,
-     "Execute a shell command."},    
+    {"get_simple_dedx",  get_simple_dedx, METH_VARARGS}, 
+    {"get_dedx",  get_dedx, METH_VARARGS}, 
     {NULL, NULL, 0, NULL}       
 };
 PyMODINIT_FUNC
