3616a71c3dfe991a77f7033f322c1a6439fb98ad
Niels Bassler <niels.bassler@gmail.com>
2012-06-20 20:08:52 +0000

    Fix installer error, when installing python bindings on a clean system.

--- python/setup.py
+++ python/setup.py
@@ -1,21 +1,19 @@
+#!/usr/bin/python
 from distutils.core import setup, Extension
 
 module1 = Extension('libdedx',
                     define_macros = [('MAJOR_VERSION', '1'),
-                                     ('MINOR_VERSION', '0')],
-                    include_dirs = ['/usr/local/include'],
+                                     ('MINOR_VERSION', '2')],
+                    include_dirs = ['/home/bassler/Projects/libdedx/trunk/libdedx'],
                     libraries = ['dedx'],
-                    library_dirs = ['/usr/local/lib'],
+                    library_dirs = ['/home/bassler/Projects/libdedx/trunk/libdedx'],
                     sources = ['libdedx.c'])
-
 setup (name = 'libdedx',
-       version = '1.0',
+       version = '1.2.0-svn215',
        description = 'Wrapper for stopping power library libdedx',
        author = 'Jakob Toftegaard',
        author_email = 'jakob.toftegaard@gmail.com',
        url = 'http://libdedx.sourceforge.net/',
        long_description = '',
        ext_modules = [module1])
-
-
 
