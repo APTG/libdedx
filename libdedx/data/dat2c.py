5c9beeb1e09622f37fc3b96ea5cb75d55a628c1e
Niels Bassler <niels.bassler@gmail.com>
2013-03-12 20:00:37 +0000

    Add more data headers.

--- libdedx/data/dat2c.py
+++ libdedx/data/dat2c.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
-
+#
 # Convert a ascii file to C code which can be included in headers.
+#
+# 12.03.2013 Niels Bassler <bassler@phys.au.dk>
+#
 
 import numpy as np
-
 
 def write2d(_data, _name, _bsize, _nsize): #
     i = 1
@@ -53,7 +54,7 @@ def write2d(_data, _name, _bsize, _nsize): #
         k += 1
 
         # check if end of line
-        if i >= 7:
+        if i >= 6:
             i = 0
             print ""            
         i += 1
@@ -88,27 +89,60 @@ def write1d(_data, _name, _bsize): #
 
 
         # check if end of line
-        if i >= 7:
+        if i >= 6:
             i = 0
             print ""            
         i += 1
         
 
+#### ASTAR ####
+#data = np.loadtxt("astarEng.dat")
+#write1d(data[1:],"static const float astar_energy",122)  # 122 = data[0]
+#data = np.loadtxt("ASTAR.dat")
+#write2d(data,"static const float astar",122,74)
 
+### ESTAR ###
+#data = np.loadtxt("estarEng.dat")
+#write1d(data[1:],"static const float estar_energy",133)
+#data = np.loadtxt("ESTAR.dat")
+#write2d(data,"static const float estar",132,77)
 
-bsize = 122
-nsize = 2
+### PSTAR ###
+#data = np.loadtxt("pstarEng.dat")
+#write1d(data[1:],"static const float pstar_energy",133)
+#data = np.loadtxt("PSTAR.dat")
+#write2d(data,"static const float pstar",133,74)
 
-data = np.loadtxt("ASTAR.dat")
+### MSTAR ###
+#data = np.loadtxt("mstarEng.dat")
+#write1d(data[1:],"static const float mstar_energy",132)
+#data = np.loadtxt("MSTAR.dat")
+#write2d(data,"static const float mstar",132,78)
 
-write2d(data,"foobar",bsize,nsize)
+### ICRU73 ###
+#data = np.loadtxt("icru73Eng.dat")
+#write1d(data[1:],"static const float icru73_energy",53)
+#data = np.loadtxt("ICRU73.dat")
+#write2d(data,"static const float icru73",53,896)
 
-#for ii in range(nsize):
-#    write1d(data[ii*bsize:],"foobar_"+str(ii),bsize)
+### ICRU73_NEW ###
+#data = np.loadtxt("icru73_newEng.dat")
+#write1d(data[1:],"static const float icru73new_energy",53)
+#data = np.loadtxt("ICRU73_NEW.dat")
+#write2d(data,"static const float icru73new",53,36)
+
+### ICRU49 PROTONS ###
+#data = np.loadtxt("icru_pstarEng.dat")
+#write1d(data[1:],"static const float icru_pstar_energy",133)
+#data = np.loadtxt("ICRU_PSTAR.dat")
+#write2d(data,"static const float icru_pstar",133,74)
     
-
-
-
-
-    
-
+### ICRU49 ALPHAS ###
+#data = np.loadtxt("icru_astarEng.dat")
+#write1d(data[1:],"static const float icru_astar_energy",122)
+#data = np.loadtxt("ICRU_ASTAR.dat")
+#write2d(data,"static const float icru_astar",122,74)
+   
+### BETHE energy grid (what is this used for anyway?)  ###
+data = np.loadtxt("betheEng.dat")
+write1d(data[1:],"static const float bethe_energy",122)
