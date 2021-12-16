83541643c9f8978af0e01f3af761e3e8f297825d
Leszek Grzanka <leszek.grzanka@gmail.com>
2021-12-16 19:03:48 +0100

    test

--- examples/dedx_use_wrappers.c
+++ examples/dedx_use_wrappers.c
@@ -5,14 +5,14 @@
 
 int main() {
     int err = 0;
-    float energies[100];
-    float results[100];
+    float energies[10];
+    float results[10];
     int program = DEDX_BETHE_EXT00;
     int ion = DEDX_HYDROGEN;
     int target = DEDX_WATER_LIQUID;
-    int no_of_points = 100;
-    for (int i = 1; i < no_of_points + 1; i++) {
-        energies[i - 1] = i * 1.1f;
+    int no_of_points = 10;
+    for (int i = 0; i < no_of_points; i++) {
+        energies[i] = 1.f + i * 1.1f;
     }
 
     err = dedx_get_stp_table(program, ion, target, no_of_points, energies, results);
