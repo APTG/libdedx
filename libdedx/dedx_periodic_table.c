e5b6f7b2d44c07352e2a68a85124386314a3bf92
Leszek Grzanka <leszek.grzanka@gmail.com>
2022-04-01 18:16:49 +0200

    fixes error in number of nucleon calculation

--- libdedx/dedx_periodic_table.c
+++ libdedx/dedx_periodic_table.c
@@ -1,18 +1,25 @@
 #include "dedx_periodic_table.h"
-float _dedx_get_atom_charge(int id, int *err)
-{
-    if(id < 113)
-    {
-      return id;
+
+float _dedx_get_atom_charge(int id, int *err) {
+    if (id < 113) {
+        return id;
     }
     *err = 204;
     return -1;
 }
-float _dedx_get_atom_mass(int id, int *err)
-{
+
+float _dedx_get_atom_mass(int id, int *err) {
     *err = 0;
-    if(id < 113)
-      return dedx_amu[id-1];
+    if (id < 113)
+        return dedx_amu[id - 1];
+    *err = 204;
+    return -1;
+}
+
+int _dedx_get_nucleon(int id, int *err){
+    *err = 0;
+    if (id < 113)
+        return dedx_nucl[id - 1];
     *err = 204;
     return -1;
 }
