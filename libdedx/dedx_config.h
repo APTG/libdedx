f3916271ac99f993657779d8db69add2d3089e14
Niels Bassler <niels.bassler@gmail.com>
2026-03-28 18:09:27 +0100

    more clang fixes

--- libdedx/dedx_config.h
+++ libdedx/dedx_config.h
@@ -1,6 +1,8 @@
 // the configured options and settings for DEDX
-#define DEDX_VERSION_MAJOR @DEDX_VERSION_MAJOR @
-#define DEDX_VERSION_MINOR @DEDX_VERSION_MINOR @
-#define DEDX_VERSION_PATCH @DEDX_VERSION_PATCH @
+// clang-format off
+#define DEDX_VERSION_MAJOR @DEDX_VERSION_MAJOR@
+#define DEDX_VERSION_MINOR @DEDX_VERSION_MINOR@
+#define DEDX_VERSION_PATCH @DEDX_VERSION_PATCH@
+// clang-format on
 #define DEDX_DATA_PATH "/usr/local/share/libdedx/"
 #define DEDX_DATA_PATH_LOCAL "data/"
