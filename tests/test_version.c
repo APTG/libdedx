4256b98662821c4f03526eb565fc0e24e9d70353
Niels Bassler <niels.bassler@gmail.com>
2026-04-01 15:37:44 +0200

    Update build configuration for Release mode and clean up version test

--- tests/test_version.c
+++ tests/test_version.c
@@ -1,6 +1,5 @@
 #include <dedx.h>
 #include <stdio.h>
-#include <string.h>
 
 int main(void) {
     int major = 0;
@@ -10,10 +9,6 @@ int main(void) {
 
     dedx_get_version(&major, &minor, &patch);
     version = dedx_get_version_string();
-    if (version == NULL || strlen(version) == 0) {
-        fprintf(stderr, "version string is empty\n");
-        return 1;
-    }
     printf("%d.%d.%d\n", major, minor, patch);
     printf("%s\n", version);
     return 0;
